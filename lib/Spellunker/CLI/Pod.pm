c3d4514b783b51967e2442e910aa958906dc8de1
tokuhirom <tokuhirom@gmail.com>
2013-04-16 23:26:28 +0900

    Checking in changes prior to tagging of version v0.0.4.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 9bd17b2..94f83b3 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Test-Spelling-Lite
    
     {{$NEXT}}
    
    +v0.0.4 2013-04-16T14:25:25Z
    +
    +    - ignore "isn't"
    +    - more stopwords
    +    - Fixed user dictionary loader.
    +
     v0.0.3 2013-04-16T12:17:07Z
    
         - Fixed Test::Spellunker bug.

--- lib/Spellunker/CLI/Pod.pm
+++ lib/Spellunker/CLI/Pod.pm
@@ -5,7 +5,7 @@ use utf8;
 use Getopt::Long;
 use Spellunker::Pod;
 
-use version; our $VERSION = version->declare("v0.0.3");
+use version; our $VERSION = version->declare("v0.0.4");
 
 sub new {
     my $class = shift;
