bc63496a99d911f86db81a4879db76153d928c7d
tokuhirom <tokuhirom@gmail.com>
2013-04-17 10:54:18 +0900

    Checking in changes prior to tagging of version v0.0.5.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 94f83b3..88ad82b 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Test-Spelling-Lite
    
     {{$NEXT}}
    
    +v0.0.5 2013-04-17T01:54:02Z
    +
    +    - suppress uuv on win32
    +
     v0.0.4 2013-04-16T14:25:25Z
    
         - ignore "isn't"

--- lib/Spellunker/CLI/Pod.pm
+++ lib/Spellunker/CLI/Pod.pm
@@ -5,7 +5,7 @@ use utf8;
 use Getopt::Long;
 use Spellunker::Pod;
 
-use version; our $VERSION = version->declare("v0.0.4");
+use version; our $VERSION = version->declare("v0.0.5");
 
 sub new {
     my $class = shift;
