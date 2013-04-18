a0611d9cf389f03cb5bf2e74c81e37ea2cee0284
tokuhirom <tokuhirom@gmail.com>
2013-04-18 12:14:28 +0900

    Checking in changes prior to tagging of version v0.0.11.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a8222d9..8580005 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Test-Spelling-Lite
    
     {{$NEXT}}
    
    +v0.0.11 2013-04-18T03:14:00Z
    +
    +    - Support "You'd"
    +      (Suggested by gfx++)
    +    - Depend *on* Pod::Simple 3.26
    +
     v0.0.10 2013-04-18T02:58:08Z
    
         - Do not include user dictionary in testing Spellunker itself.

--- lib/Test/Spellunker.pm
+++ lib/Test/Spellunker.pm
@@ -7,7 +7,7 @@ use Spellunker::Pod;
 
 use parent qw(Exporter);
 
-use version; our $VERSION = version->declare("v0.0.10");
+use version; our $VERSION = version->declare("v0.0.11");
 
 use Test::Builder;
 use File::Spec;
