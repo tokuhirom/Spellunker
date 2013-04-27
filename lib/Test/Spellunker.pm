d1ef57150f7adff413adfcd8e664a66fc48545ae
tokuhirom <tokuhirom@gmail.com>
2013-04-27 23:16:51 +0900

    Checking in changes prior to tagging of version v0.2.4.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a4e4bce..377d922 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl extension Spellunker
    
     {{$NEXT}}
    
    +v0.2.4 2013-04-27T14:15:11Z
    +
    +    - Added more stopwords.
    +      (tokuhirom)
    +    - Support &amp;.
    +      (tokuhirom)
    +
     v0.2.3 2013-04-24T10:11:05Z
    
         - Ignore multi byte characters

--- lib/Test/Spellunker.pm
+++ lib/Test/Spellunker.pm
@@ -7,7 +7,7 @@ use Spellunker::Pod;
 
 use parent qw(Exporter);
 
-use version; our $VERSION = version->declare("v0.2.3");
+use version; our $VERSION = version->declare("v0.2.4");
 
 use Test::Builder;
 use File::Spec;
