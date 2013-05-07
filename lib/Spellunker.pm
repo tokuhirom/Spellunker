8ffb097b206ce2c11648a8fbc90373186119e32d
tokuhirom <tokuhirom@gmail.com>
2013-05-07 12:19:54 +0900

    Checking in changes prior to tagging of version v0.2.5.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 377d922..102b608 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Spellunker
    
     {{$NEXT}}
    
    +v0.2.5 2013-05-07T03:19:25Z
    +
    +    - Added more stopwords.
    +
     v0.2.4 2013-04-27T14:15:11Z
    
         - Added more stopwords.

--- lib/Spellunker.pm
+++ lib/Spellunker.pm
@@ -4,7 +4,7 @@ use warnings FATAL => 'all';
 use utf8;
 use 5.008001;
 
-use version; our $VERSION = version->declare("v0.2.4");
+use version; our $VERSION = version->declare("v0.2.5");
 
 use Scalar::Util ();
 use File::Spec ();
