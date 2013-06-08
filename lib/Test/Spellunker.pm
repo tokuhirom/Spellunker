7cff52e65c8d36ac3a476c4ca914c3ee97cfac93
tokuhirom <tokuhirom@gmail.com>
2013-06-08 13:49:00 +0900

    Checking in changes prior to tagging of version v0.2.9.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 5a14a17..ed6859f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Spellunker
    
     {{$NEXT}}
    
    +v0.2.9 2013-06-08T04:48:12Z
    +
    +    - more stopwords
    +      (tokuhirom)
    +
     v0.2.8 2013-05-26T23:28:34Z
    
         - Show correct test line number if all_pod_files_spelling_ok fails.

--- lib/Test/Spellunker.pm
+++ lib/Test/Spellunker.pm
@@ -7,7 +7,7 @@ use Spellunker::Pod;
 
 use parent qw(Exporter);
 
-use version; our $VERSION = version->declare("v0.2.8");
+use version; our $VERSION = version->declare("v0.2.9");
 
 use Test::Builder;
 use File::Spec;
